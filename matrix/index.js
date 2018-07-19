1b6397aab2fc05a6aad6c5e36193837db03b416e
Dennis <dzianis.kachanau@pepperstone.com>
2018-07-19 14:10:23 +1000

    Implement Spirals

--- matrix/index.js
+++ matrix/index.js
@@ -14,7 +14,73 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+function createMatrix(n, m) {
+  const matrix = []
+  
+  for (let i = 0; i < n; i++) {
+    matrix.push([])
+    
+    for(let j = 0; j < m; j++) {
+      matrix[i].push(undefined)
+    }
+  }
+  
+  return matrix
+}
 
-function matrix(n) {}
+function printMatrix(matrix) {
+  for (let i = 0; i < matrix.length; i++) {
+    console.log(matrix[i])
+  }
+}
+
+
+// direction: -1 | +1
+function fillRow(matrix, counter, pos, direction) {
+  const {row, col} = pos
+  let i = col
+  
+  while(i < matrix[row].length && i >= 0 && isNaN(matrix[row][i])) {
+    matrix[row][i] = counter
+    counter++
+    pos.col = i
+    i = i + direction
+  }
+  
+  return counter
+}
+
+function fillCol(matrix, counter, pos, direction) {
+  const {row, col} = pos
+  let i = row
+  
+  while(i < matrix.length && i >= 0 && isNaN(matrix[i][col])) {
+    matrix[i][col] = counter
+    counter++
+    pos.row = i
+    i = i + direction
+  }
+  
+  return counter
+}
+
+function matrix(n) {
+  let pos = {row: 0, col: 0};
+  let counter = 1;
+  const aMatrix = createMatrix(n, n)
+  
+  while (counter <= n * n) {
+    counter = fillRow(aMatrix, counter, pos, +1)
+    pos.row += 1
+    counter = fillCol(aMatrix, counter, pos, +1)
+    pos.col -= 1
+    counter = fillRow(aMatrix, counter, pos, -1)
+    pos.row -= 1
+    counter = fillCol(aMatrix, counter, pos, -1)
+    pos.col += 1
+  }
+  
+  return aMatrix
+}
 
 module.exports = matrix;
